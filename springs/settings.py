b9d0ca16fc885246907aad3c9a0af05127638816
t4christ <bakaretemitayo7@gmail.com>
2019-07-27 15:10:40 +0100

    springsoflife

--- springs/settings.py
+++ springs/settings.py
@@ -97,12 +97,13 @@ WSGI_APPLICATION = 'springs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 import dj_database_url
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
