e18c568131c4f07a027d4eea85b8463e12c04961
t4christ <bakaretemitayo712@gmail.com>
2020-06-30 18:28:02 +0100

    modify dockerun file

---
+++ test.sh
@@ -0,0 +1,16 @@
+
+# echo "File Name: $0"
+# echo "First Parameter : $1"
+# echo "Second Parameter : $2"
+# echo "Third Parameter : $3"
+# echo "Quoted Values: $@"
+# echo "Quoted Values: $*"
+# echo "Total Number of Parameters : $#"
+
+
+for TOKEN in $*
+do
+   echo $TOKEN
+done
+
+echo 'Echoing' $? #exit status
