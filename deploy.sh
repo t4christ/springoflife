9d5e431f3ea196720667ffe62e04bb8ce9853dbc
tdrohealth <tbakare@drohealth.com>
2021-06-30 17:00:10 +0100

    modify build file

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ echo "Running Aws Version"
 docker run --rm -it amazon/aws-cli --version
 
 
-# echo "Running deploy script"
+echo "Running deploy script"
 
-# aws ssm send-command \
-#     --document-name "AWS-RunRemoteScript" \
-#     --targets "Key=instanceids,Values=i-0881ef84752782ea2" \
-#     --parameters '{"sourceType":["S3"],"sourceInfo":["{\"path\":\"https://s3.amazonaws.com/springs-bucket/scripts/shell/deploy.sh\"}"],"commandLine":["deploy.sh"]}'
+docker run --rm -it amazon/aws-cli ssm send-command \
+    --document-name "AWS-RunRemoteScript" \
+    --targets "Key=instanceids,Values=i-0881ef84752782ea2" \
+    --parameters '{"sourceType":["S3"],"sourceInfo":["{\"path\":\"https://s3.amazonaws.com/springs-bucket/scripts/shell/deploy.sh\"}"],"commandLine":["deploy.sh"]}'
