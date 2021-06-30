0c642188c0800f955db9bfc65f3a995499ab0acc
tdrohealth <tbakare@drohealth.com>
2021-06-30 15:21:13 +0100

    modify build file

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+aws ssm send-command \
+    --document-name "AWS-RunRemoteScript" \
+    --targets "Key=instanceids,Values=i-0881ef84752782ea2" \
+    --parameters '{"sourceType":["S3"],"sourceInfo":["{\"path\":\"https://s3.amazonaws.com/springs-bucket/scripts/shell/deploy.sh\"}"],"commandLine":["deploy.sh"]}'
