d5fba97bb410c9234557e84fe52ca439329a3bcb
tdrohealth <tbakare@drohealth.com>
2021-06-30 16:16:32 +0100

    modify build file

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ echo "Installing Aws"
 ./aws/install
 
 
-echo "Running deploy script"
+# echo "Running deploy script"
 
-aws ssm send-command \
-    --document-name "AWS-RunRemoteScript" \
-    --targets "Key=instanceids,Values=i-0881ef84752782ea2" \
-    --parameters '{"sourceType":["S3"],"sourceInfo":["{\"path\":\"https://s3.amazonaws.com/springs-bucket/scripts/shell/deploy.sh\"}"],"commandLine":["deploy.sh"]}'
+# aws ssm send-command \
+#     --document-name "AWS-RunRemoteScript" \
+#     --targets "Key=instanceids,Values=i-0881ef84752782ea2" \
+#     --parameters '{"sourceType":["S3"],"sourceInfo":["{\"path\":\"https://s3.amazonaws.com/springs-bucket/scripts/shell/deploy.sh\"}"],"commandLine":["deploy.sh"]}'
