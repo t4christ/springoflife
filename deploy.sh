3f5a7605dcb359ad3cb237f650ef2e4940374fc6
tdrohealth <tbakare@drohealth.com>
2021-06-30 21:40:36 +0100

    modify build file

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-echo "Running Aws Version"
+echo "Running Docker Compose"
+docker-compose up
 
-docker run --rm -it amazon/aws-cli --version
-
-
-echo "Running deploy script"
-
-docker run --rm -it amazon/aws-cli ssm send-command \
-    --document-name "AWS-RunRemoteScript" \
-    --targets "Key=instanceids,Values=i-0881ef84752782ea2" \
-    --parameters '{"sourceType":["S3"],"sourceInfo":["{\"path\":\"https://s3.amazonaws.com/springs-bucket/scripts/shell/deploy.sh\"}"],"commandLine":["deploy.sh"]}'
--- test.sh
+++ test.sh
@@ -8,9 +8,17 @@
 # echo "Total Number of Parameters : $#"
 
 
-for TOKEN in $*
-do
-   echo $TOKEN
-done
+# for TOKEN in $*
+# do
+#    echo $TOKEN
+# done
 
-echo 'Echoing' $? #exit status
+# echo 'Echoing' $? #exit status
+
+
+
+# $ ssh-keygen -t rsa -b 4096 -C 'build@travis-ci.org' -f ./deploy_rsa
+# $ travis encrypt-file deploy_rsa --add
+# $ ssh-copy-id -i deploy_rsa.pub ec2-user@<your-ec2-instance>
+# $ rm -f deploy_rsa deploy_rsa.pub
+# $ git add deploy_rsa.enc
