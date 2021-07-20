ae3860728a5a1af1152d44452204b4bdb17d9ab0
tdrohealth <tbakare@drohealth.com>
2021-07-20 13:42:47 +0100

    update homepage

--- springs/settings.py
+++ springs/settings.py
@@ -154,7 +154,7 @@ DATABASES = {
             'NAME': os.environ.get('POSTGRES_DATABASE'),
             'USER': os.environ.get('POSTGRES_USER'),
             'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-            'HOST': os.environ.get('POSTGRES_HOST','db'),
+            'HOST': os.environ.get('POSTGRES_HOST'),
             'PORT': os.environ.get('POSTGRES_PORT','5432'),
         }
     }
