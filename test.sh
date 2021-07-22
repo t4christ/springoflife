4a82d0f65b9b01bc046064de6e896dfe67241075
tdrohealth <tbakare@drohealth.com>
2021-07-22 19:30:08 +0100

    update nginx

--- test.sh
+++ test.sh
@@ -55,3 +55,6 @@
 # Two Database Clustering Form
 # Shared Nothing Architecture
 # Shared Disk Architecture
+
+
+#  openssl req -subj '/CN=localhost' -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365
